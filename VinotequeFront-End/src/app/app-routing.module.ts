8c52a035e7702d007e817870c95966bd91a3b5d8
GiVill <vlllgu01e10m208q@studenti.unical.it>
2023-01-05 21:49:15 +0100

    prova routing

--- VinotequeFront-End/src/app/app-routing.module.ts
+++ VinotequeFront-End/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WineViewComponent } from './Components/wine-view/wine-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'shop', component:WineViewComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
