b8358f7105f8097962e24933f5190f112790b87c
SEBASTIANO <Sebastiano@SEBASTIANO>
2023-01-09 17:36:18 +0100

    Registrazione

--- VinotequeFront-End/src/app/app-routing.module.ts
+++ VinotequeFront-End/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginCardComponent } from './Components/login-card/login-card.component';
+import { RegistrationCardComponent } from './Components/registration-card/registration-card.component';
 import { ShopPageComponent } from './Components/shop-page/shop-page.component';
 
 const routes: Routes = [
   { path:'shop', component:ShopPageComponent},
-  { path:'login', component:LoginCardComponent}
+  { path:'login', component:LoginCardComponent},
+  { path: 'registration', component:RegistrationCardComponent},
 ];
 
 @NgModule({
