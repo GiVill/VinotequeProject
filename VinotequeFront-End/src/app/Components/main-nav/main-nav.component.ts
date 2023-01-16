43b779958700f70bd680650dfcc4b860e1d831b9
GiVill <vlllgu01e10m208q@studenti.unical.it>
2023-01-16 17:32:32 +0100

    auth

--- VinotequeFront-End/src/app/Components/main-nav/main-nav.component.ts
+++ VinotequeFront-End/src/app/Components/main-nav/main-nav.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/Services/authentication.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-main-nav',
   templateUrl: './main-nav.component.html',
   styleUrls: ['./main-nav.component.css']
 })
-export class MainNavComponent {
+export class MainNavComponent implements OnInit,AfterViewChecked{
+
+  logged : Boolean = false;
+  sommelier : Boolean = false;
+  admin : Boolean = false;
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
@@ -17,7 +23,23 @@ export class MainNavComponent {
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver,
+              private service : AuthenticationService,
+              private _snackBar: MatSnackBar) {}
+
+  ngOnInit(): void {
+    this.logged = this.service.isLogged();
+  }
+
+  cartAction(){
+    if(this.logged){
+      console.log("ciao")
+    } else {
+      this._snackBar.open("Non sei loggato!", "OK");
+    }
+
+
+  }
 
   loginRedirect(){
     window.location.href = 'http://localhost:8080/login.html';
